14c6b74827dff92ca880d2f262a07bee54a5980b
Jaime <jriverarojas@gmail.com>
2024-08-30 20:54:47 -0400

    first commit

---
+++ src/messenger/services/redis.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import Redis from 'ioredis';
+import { ConfigService } from '@nestjs/config'
+
+@Injectable()
+export class RedisService {
+    private readerRedis: Redis;
+    private writerRedis: Redis;
+    constructor(private configService: ConfigService) {
+        this.readerRedis = new Redis({
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+            password: configService.get<string>('REDIS_PASSWORD'),
+        });
+
+        this.writerRedis = new Redis({
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+            password: configService.get<string>('REDIS_PASSWORD'),
+        });
+        console.log('CONNECTED TO REDIS!!!!')
+    }
+
+    async addToQueue(data: any): Promise<string> {
+        const id = new Date().getTime().toString();//Generate unique id
+        await this.writerRedis.lpush('queue3', JSON.stringify({ id, ...data }));
+        return id;
+    }
+
+
+}
